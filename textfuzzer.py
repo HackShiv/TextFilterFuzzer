726163d7da8f24bb2d8676618ac756293c514dc1
HackerShiv <107373873+HackShiv@users.noreply.github.com>
2024-05-08 11:47:32 +0100

    Add files via upload

---
+++ textfuzzer.py
@@ -0,0 +1,33 @@
+import requests
+import concurrent.futures
+from colorama import Fore, Style
+
+session = requests.Session()
+
+def make_request(url, word, filter_texts):
+    try:
+        response = session.get(f"{url}/{word}", timeout=5)
+        if all(filter_text not in response.text for filter_text in filter_texts):
+            print(f"{url}/{word}", Fore.CYAN + str(response.status_code) + Style.RESET_ALL)
+    except requests.RequestException as e:
+        print(f"Error making request for {url}/{word}: {e}")
+
+def fuzz_url(url, wordlist, filter_texts):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(make_request, url, word, filter_texts) for word in wordlist]
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+
+def main():
+    url = input("Enter the URL: ")
+    wordlist_path = input("Enter the path to the wordlist file: ")
+    filter_text_input = input("Enter the filter text separated by comma (e.g., 'Not Found,406 Not Allowed'): ")
+    filter_texts = [filter_text.strip() for filter_text in filter_text_input.split(",")]
+
+    with open(wordlist_path, 'r') as file:
+        words = file.read().split()
+
+    fuzz_url(url, words, filter_texts)
+
+if __name__ == "__main__":
+    main()
